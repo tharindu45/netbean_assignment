52709a6137517fe488a4a9dc7955671201ffe7c4
tharindu45 <prasadtharindu816@gmail.com>
2020-11-05 21:47:38 -0800

    create netbeans project

---
+++ src/project1/Project1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tharindu
+ */
+public class Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner num=new Scanner(System.in);
+        int price=40;
+        System.out.println("Enter the age");
+        int age=num.nextInt();
+        System.out.println("Enter your country name");
+        String country=num.next();
+        if(country.equals("warren"))
+        {
+        }
+        if(age<5)
+        {
+            price=0;
+        }
+        if(age>=65)
+        {
+           price=price-price*50/100; 
+           System.out.println("price :$"+price+".00");
+        }
+        else
+            System.out.println("$"+price+".00");
+        
+        // TODO code application logic here
+    }
+    
+}
