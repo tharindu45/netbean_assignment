b8d687bd5632092ec24934257a2e267ffd6386c9
tharindu45 <prasadtharindu816@gmail.com>
2020-11-15 20:13:30 -0800

    finish second part and third part

--- src/project1/Project1.java
+++ src/project1/Project1.java
@@ -18,49 +18,67 @@ public class Project1 {
     public static void main(String[] args) {
         
         Scanner num=new Scanner(System.in);
-        int price=40;
+        double price=40.00;
         System.out.println("Enter the age");
         int age=num.nextInt();
         
-        if(age<5)
+        if(age>0)
         {
-            price=0;
-             System.out.println("price :$"+price+".00");
-        }
-        else{
-            System.out.println("Enter your country name");
-            String country=num.next();
-            
-            if(country.equals("warren"))
+            if(age<5)
             {
-                price=30;
-                if(age>=65)
-                {
-                   price=price-price*50/100; 
-                   System.out.println("price :$"+price+".00");
-                }
-                else
-                    System.out.println("$"+price+".00");
-
+                price=0.00;
+                 
             }
-            else{
-                if(age>=65)
-                {
-                   price=price-price*50/100; 
-                   System.out.println("price :$"+price+".00");
-                }
-                else
-                    System.out.println("$"+price+".00");
-
-        
-        }
-        
             
+            else{
+                System.out.println("Enter your country name");
+                String country1=num.next();
+
+                String country= country1.toLowerCase();
+                
+                
+                if(country.equals("clermont") && age<14){
+                    price=price-price*18/100;
+                }
+                
+                else if(age>=65)
+                {
+                    if(country.equals("warren")){
+                     price=30.00;
+                     
+                     price=price-price*50/100;
+                    }
+                    else if(country.equals("campbell"))
+                    {
+                      price=price-price*57.5/100;
+                     
+                    }
+                    else
+                        price=price-price*50/100;
+                    
+                }
+                else if(country.equals("warren"))
+                {
+                    price=30.00;
+                }
+                        
+            }
+          System.out.println("ticket price: $"+price+"0");     
         }
+        
+       
+        else{
+                System.out.println("Invalid age please input again correcttly ");
+            }
+            
+    }
+        
+        
+        
         
         
         
         // TODO code application logic here
-    }
-    
 }
+    
+
